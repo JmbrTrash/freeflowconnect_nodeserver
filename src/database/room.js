245c6848ee81d78637f029daf495e82ac7e7170f
Mathias <mathias.de.weerdt@gmail.com>
2019-10-07 12:12:59 +0200

    Stable version

--- src/database/room.js
+++ src/database/room.js
@@ -13,26 +13,35 @@ class RoomList {
         return instance;
     }
     addRoom(name) {
-        console.log("adding room");
         if (this.rooms[name] == null) {
+            console.log("Adding room: " + name);
             this.rooms[name] = { users: [] };
         }
     }
     joinRoom(roomName, userName) {
         if (this.rooms[roomName] == null) {
-            console.log("joining room but null");
-            this.addRoom(roomName);
+            console.log("joining room: " + roomName + " but room does not exist adding room ... ");
+
+            if(roomName !== undefined && roomName !== null) {
+                this.addRoom(roomName);
+            }
         }
-        this.rooms[roomName].users.push(userName);
+
+        if(this.rooms[roomName] != null && userName !== undefined && userName !== null) {
+            this.rooms[roomName].users.push(userName);             
+        }
     }
     leaveRoom(roomName, username) {
-        console.log("Deleting: " + username)
-        console.log(this.rooms[roomName])
-        this.rooms[roomName].users.splice(this.rooms[roomName].users.indexOf(username), 1)
-        console.log(this.rooms[roomName])
+        if(this.rooms !== undefined && this.rooms !== null && roomName !== undefined && roomName !== null) {
+            console.log("Deleting: " + username)
+            this.rooms[roomName].users.splice(this.rooms[roomName].users.indexOf(username), 1)
+        } 
     }
     getRoom(name) {
-        return this.rooms[name];
+        if(this.rooms !== undefined && this.rooms !== null && 
+            name !== undefined && name !== null) {
+            return this.rooms[name];
+        }
     }
     getRooms() {
         return this.rooms;
